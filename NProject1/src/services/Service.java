62e61ddc5ae87f730c68579fca09c112e49db184
Tushar Choudhary <tchoudhary@xebia.com>
2014-01-10 18:45:24 +0530

    @Tushar	-	Service Class added

---
+++ NProject1/src/services/Service.java
@@ -0,0 +1,41 @@
+package service;
+
+import java.awt.List;
+import java.sql.Connection;
+
+public class Services {
+	private Connection connection;
+	//util.DbUtil DbUtil;
+
+	public Services() {
+
+		//connection = DbUtil.getConnection();
+	}
+
+	public Object getHighestMarksForSubject() {
+
+		return null;
+
+	}
+
+	public List getSubjectList() {
+
+		return null;
+	}
+
+	public List getCourseList() {
+
+		return null;
+	}
+
+	public List getStudentList() {
+
+		return null;
+	}
+
+	public List getPiechartOutputs() {
+
+		return null;
+	}
+
+}
