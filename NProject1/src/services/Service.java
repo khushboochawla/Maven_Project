9b1432add151765b734d43d8098d16b5e65b46af
Vivek Negi <vnegi@xebia.com>
2014-01-11 22:03:38 +0530

    @vivek Add two methods definition of services.java

--- NProject1/src/services/Service.java
+++ NProject1/src/services/Service.java
@@ -1,16 +1,24 @@
 package services;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.daniel.model.Roles;
+
 import model.Course;
+import model.Score;
 import model.Semester;
 import model.Subject;
 
 /**
  * @author Tushar Choudhary
- *
- * This class is used to interact with db to fetch and return the results
+ * 
+ *         This class is used to interact with db to fetch and return the
+ *         results
  */
 
 public class Service {
@@ -22,27 +30,64 @@ public class Service {
 		connection = DbUtil.getConnection();
 	}
 
-	public Object getHighestMarksForSubject() {
+	public List getHighestMarksForSubject(int subjectId) {
+		List<Score> marks = new ArrayList<Score>();
+		try {
+			Statement statement = connection.createStatement();
 
-		return null;
+			ResultSet rs = statement
+					.executeQuery("select sc.marks, sc.studentId from score sc join student st on sc.studentId=st.studentId where sc.subjectId="
+							+ subjectId);
+			while (rs.next()) {
+				Score scores = new Score();
+				scores.setMarks(rs.getInt("sc.marks"));
+				scores.setStudentId(rs.getInt("sc.studentId"));
+				scores.setSubId(rs.getInt("sc.subjectId"));
 
+				marks.add(scores);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return marks;
 	}
 
-	public List<Subject> getSubjectList() {
+	public List<Subject> getSubjectList(int semesterId, int courseId) {
+		List<Subject> subjects = new ArrayList<Subject>();
+		try {
+			Statement statement = connection.createStatement();
 
-		return null;
+			ResultSet rs = statement.executeQuery("select * from subject where semesterId="+semesterId+" and courseId=1"+courseId);
+
+			while (rs.next()) {
+				Subject subject = new Subject();
+				subject.setSubjectId(rs.getInt("subjectId"));
+				subject.setSubjectName(rs.getString("subjectName"));
+				subjects.add(subject);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return subjects;
+
 	}
 
 	public List<Course> getCourseList() {
 
+		//select * from course;
+		
 		return null;
 	}
 
 	public List getStudentList() {
-
+		//select * from student
 		return null;
 	}
-	public List<Semester> getSemesterList(){
+
+	public List<Semester> getSemesterList() {
+		//select * from semester;
 		return null;
 	}
 
